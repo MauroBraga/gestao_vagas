22d7c5bf31115e42072032249edefcde63670f70
mauro <mrb0305@gmail.com>
2025-03-16 19:14:07 -0300

    Validar Token

--- src/main/java/br/com/mrb/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/br/com/mrb/gestao_vagas/security/SecurityFilter.java
@@ -1,20 +1,40 @@
 package br.com.mrb.gestao_vagas.security;
 
+import br.com.mrb.gestao_vagas.providers.JWTProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTProvider jwtProvider;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
-        System.out.println(authHeader);
+        SecurityContextHolder.getContext().setAuthentication(null);
+        if(authHeader != null) {
+            var subjecyToken = this.jwtProvider.validarToken(authHeader);
+            if(subjecyToken.isEmpty()) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+            request.setAttribute("company_id",subjecyToken);
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjecyToken, null, Collections.emptyList());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
         filterChain.doFilter(request, response);
     }
 }
