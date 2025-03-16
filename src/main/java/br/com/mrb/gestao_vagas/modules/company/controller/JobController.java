50a3b35729432ecd303d7b8d36896d8051a33b6b
mauro <mrb0305@gmail.com>
2025-03-16 19:57:11 -0300

    Refatorando JobController

--- src/main/java/br/com/mrb/gestao_vagas/modules/company/controller/JobController.java
+++ src/main/java/br/com/mrb/gestao_vagas/modules/company/controller/JobController.java
@@ -1,7 +1,10 @@
 package br.com.mrb.gestao_vagas.modules.company.controller;
 
+import br.com.mrb.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.mrb.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.mrb.gestao_vagas.modules.company.usecase.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/job")
@@ -18,8 +23,14 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping
-    public ResponseEntity<Object> create(@Valid  @RequestBody JobEntity entity) {
+    public ResponseEntity<Object> create(@Valid  @RequestBody CreateJobDTO dto, HttpServletRequest request) {
         try {
+            var company_id=request.getAttribute("company_id");
+
+            var entity=JobEntity.builder().benefits(dto.getBenefits())
+                    .description(dto.getDescription())
+                    .companyId(UUID.fromString(company_id.toString()))
+                    .levels(dto.getLevel()).build();
             var retorno = this.createJobUseCase.execute(entity);
             return ResponseEntity.ok(retorno);
         }catch (Exception e){
