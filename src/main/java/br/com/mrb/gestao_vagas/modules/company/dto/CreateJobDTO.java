6a185a72d9201aae2b12494d6c8493b2644337df
mauro <mrb0305@gmail.com>
2025-03-26 10:14:00 -0300

    Adicionando Swagger

--- src/main/java/br/com/mrb/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/mrb/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,10 +1,16 @@
 package br.com.mrb.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+
 public class CreateJobDTO {
+
+    @Schema(example = "Vaga para pessoa desenvolvedora Junior", requiredMode = Schema.RequiredMode.REQUIRED)
     private String description;
+    @Schema(example = "GYMPASS, Plano de Saúde", requiredMode = Schema.RequiredMode.REQUIRED)
     private String benefits;
+    @Schema(example = "Junior", requiredMode = Schema.RequiredMode.REQUIRED)
     private String level;
 }
